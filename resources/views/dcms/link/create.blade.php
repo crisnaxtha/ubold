a779573e2e79b0f44b9d6fc0599a9421203e67cf
Lochan <krishnashrestha2052@gmail.com>
2019-07-14 01:26:47 +0545

    links intregrated

--- resources/views/dcms/link/create.blade.php
+++ resources/views/dcms/link/create.blade.php
@@ -1,42 +1,35 @@
 @extends('dcms.layouts.app')
 
 @section('content')
-
+@include('dcms.includes.breadcrumb')
 <div class="row">
-        <div class="col-lg-12 col-md-12 col-xs-12">
-            <section class="panel">
-                <header class="panel-heading">
-                   <h3>{{ $_panel }}</h3>
-                </header>
-                <div class="panel-body">
-                        @include('dcms.includes.buttons.button-back')
-                        @include('dcms.includes.flash_message_error')                    
-                    <div class=" form">
-                        <?php
-                        dm_hpostform(URL::route($_base_route.'.store'), 'POST');
-                        ?>
-                        @foreach($data['lang'] as $row)
-                        <?php 
-                        dm_hidden('rows['.$loop->index.'][lang_id]', $row->id);
-                        dm_hinput('text','rows['.$loop->index.'][name]', "Name Of Link (<strong>$row->name</strong>)(*)", 'rows.'.$loop->index.'.name'); 
-                        ?>
-                        @endforeach
-                        <?php 
-                        dm_hinput('url','url', "URL of Website(*)", 'url');
-                        dm_hinput('number','order', "Order", 'order');
-                        dm_hcheckbox('checkbox', 'status', 'Status');
-                        dm_hsubmit('Submit', URL::route($_base_route.'.index'), 'Cancel');
-                        dm_closeform();
-                        ?>
-                    </div>
+    <div class="col-lg-12 col-md-12 col-xs-12">
+        <section class="card">
+            <div class="card-body">
+                    @include('dcms.includes.buttons.button-back')
+                    @include('dcms.includes.flash_message_error')
+                <div class=" form">
+                    <?php
+                    dm_hpostform(URL::route($_base_route.'.store'), 'POST');
+                    ?>
+                    @foreach($data['lang'] as $row)
+                    <?php
+                    dm_hidden('rows['.$loop->index.'][lang_id]', $row->id);
+                    dm_hinput('text','rows['.$loop->index.'][name]', "Name Of Link (<strong>$row->name</strong>)(*)", 'rows.'.$loop->index.'.name');
+                    ?>
+                    @endforeach
+                    <?php
+                    dm_hinput('url','url', "URL of Website(*)", 'url');
+                    dm_hinput('number','order', "Order", 'order');
+                    dm_hcheckbox('checkbox', 'status', 'Status');
+                    dm_hsubmit('Submit', URL::route($_base_route.'.index'), 'Cancel');
+                    dm_closeform();
+                    ?>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     </div>
-    <?php
-
-
-    ?>
+</div>
 
 @endsection
 
--- resources/views/dcms/link/edit.blade.php
+++ resources/views/dcms/link/edit.blade.php
@@ -1,22 +1,18 @@
 @extends('dcms.layouts.app')
 
 @section('content')
-
 <div class="row">
-        <div class="col-lg-12 col-md-12 col-xs-12">
-            <section class="panel">
-                <header class="panel-heading">
-                   <h3>{{ $_panel }}</h3>
-                </header>
-                <div class="panel-body">
-                        @include('dcms.includes.buttons.button-back')
-                        @include('dcms.includes.flash_message_error')                    
-                    <div class=" form">
+    <div class="col-lg-12 col-md-12 col-xs-12">
+        <section class="card">
+            <div class="card-body">
+                    @include('dcms.includes.buttons.button-back')
+                    @include('dcms.includes.flash_message_error')
+                <div class=" form">
                         <?php
                         dm_hpostform(URL::route($_base_route.'.update', ['link_unique_id' => $data['single']->link_unique_id]), 'PUT');
                         ?>
                         @foreach($data['lang'] as $row)
-                        <?php 
+                        <?php
                          if(isset($links[$loop->index]['id'])){
                             $id = $links[$loop->index]['id'];
                         }else {
@@ -30,26 +26,21 @@
                         dm_hidden('rows['.$loop->index.'][lang_id]', $row->id);
                         dm_hidden('rows['.$loop->index.'][id]', $id);
                         dm_hidden('link_unique_id', $data['single']->link_unique_id);
-                        dm_hinputUpdate('text','rows['.$loop->index.'][name]', "Name Of Link (<strong>$row->name</strong>)(*)", $name); 
+                        dm_hinputUpdate('text','rows['.$loop->index.'][name]', "Name Of Link (<strong>$row->name</strong>)(*)", $name);
                         ?>
                         @endforeach
-                        <?php 
+                        <?php
                         dm_hinputUpdate('url','url', "URL of Website(*)", $data['single']->url);
                         dm_hinputUpdate('number','order', "Order", $data['single']->order);
                         dm_hcheckbox('checkbox', 'status', 'Status', $data['single']->status);
                         dm_hsubmit('Submit', URL::route($_base_route.'.index'), 'Cancel');
                         dm_closeform();
                         ?>
-                    </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     </div>
-    <?php
-
-
-    ?>
-
+</div>
 @endsection
 
 @section('js')
