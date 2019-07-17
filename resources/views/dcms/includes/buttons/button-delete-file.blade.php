63fb4789102d7f374790bd0f0394fa0f6a70cc61
Lochan <krishnashrestha2052@gmail.com>
2019-07-18 00:04:40 +0545

    icon delete

--- resources/views/dcms/includes/buttons/button-delete-file.blade.php
+++ resources/views/dcms/includes/buttons/button-delete-file.blade.php
@@ -1,3 +1,3 @@
 @if(Route::has($_base_route.'.destroyFile'))
-    <button  id="delete" data-id="{{ $row->id }}" data-url="{{ URL::route($_base_route.'.destroyFile', ['id'=>$row->id]) }}" type="button" class="btn btn-round btn-danger"><i class="fa fa-trash-o"></i></button>
+    <button  id="delete" data-id="{{ $row->id }}" data-url="{{ URL::route($_base_route.'.destroyFile', ['id'=>$row->id]) }}" type="button" class="btn btn-round btn-danger"><i class="far fa-trash-alt"></i></button>
 @endif
