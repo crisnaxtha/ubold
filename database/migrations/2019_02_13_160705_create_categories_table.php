817e775268eb021322ecf42fe10559e33943a5ad
Lochan <krishnashrestha2052@gmail.com>
2019-08-01 00:05:42 +0545

    working on category

--- database/migrations/2019_02_13_160705_create_categories_table.php
+++ database/migrations/2019_02_13_160705_create_categories_table.php
@@ -16,10 +16,15 @@ class CreateCategoriesTable extends Migration
             Schema::create('categories', function (Blueprint $table) {
                 $table->increments('id');
                 $table->unsignedInteger('parent_id')->nullable();
+                $table->unsignedInteger('order')->nullable();
+                $table->string('category_unique_id');
+                $table->string('icon')->nullable();
+                $table->string('color')->nullable();
                 $table->string('name');
                 $table->string('slug');
                 $table->text('description')->nullable();
                 $table->unsignedInteger('category_post_count')->default(0);
+                $table->boolean('featured')->nullable();
                 $table->timestamps();
         });
     }
