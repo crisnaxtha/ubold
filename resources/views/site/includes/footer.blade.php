19329fce33e6dddb213b4e64130a09a23132b8b9
Lochan <krishnashrestha2052@gmail.com>
2019-08-15 23:54:41 +0545

    footer intregrated in front end

--- resources/views/site/includes/footer.blade.php
+++ resources/views/site/includes/footer.blade.php
@@ -6,83 +6,25 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-3 col-md-6 col-sm-6 contact_person">
-            <h4>सम्पर्क ब्यक्ति </h4>
+            <h4>{{ $data['common']->footer_first_title }}</h4>
 
-            <div class="row">
-              <div class="col-lg-4 col-md-12 pad-right-0 wow fadeInUp" data-wow-duration="2s" data-wow-delay=".2s">
-                <figure>
-                  <img src="{{asset('assets/site/assets/images/upasachib.jpg')}}" alt="image" class="img-fluid">
-                </figure>
-              </div>
-              <div class="col-lg-8 col-md-12 wow fadeInUp" data-wow-duration="2s" data-wow-delay=".2s">
-                <ul>
-                  <li>उमाकान्त अधिकारी</li>
-                  <li>सह सचिव (प्रवक्ता)</li>
-                  <li>umakanta321@gmail.com</li>
-                  <li>कार्यालय फोन नं : ०१-४४७७७१०</li>
-                </ul>
-              </div>
-              <div class="clearfix"></div>
-            </div>
-            <hr/>
-            <div class="row">
-              <div class="col-lg-4 col-md-12 pad-right-0 wow fadeInUp" data-wow-duration="2s" data-wow-delay=".2s">
-                <figure>
-                  <img src="{{asset('assets/site/assets/images/sachibmoha.jpg') }}" alt="image" class="img-fluid">
-                </figure>
-              </div>
-              <div class="col-lg-8 col-md-12 wow fadeInUp" data-wow-duration="2s" data-wow-delay=".2s">
-                <ul>
-                  <li>प्रेम कुमार राई</li>
-                  <li>उप सचिव</li>
-                  <li>premkumarrai@gmail.com</li>
-                  <li>कार्यालय फोन नं : ०१-४४७७७१०</li>
-                </ul>
-              </div>
-              <div class="clearfix"></div>
-            </div>
+            {!! $data['common']->footer_first_title !!}
+                <div class="clearfix"></div>
 
+
+             </div>
+          <div class="col-lg-3 col-md-6 col-sm-6">
+            <h4>{{ $data['common']->footer_second_title }}</h4>
+            {!! $data['common']->footer_second_description !!}
           </div>
           <div class="col-lg-3 col-md-6 col-sm-6">
-            <h4>महत्त्वपूर्ण लिंकहरु</h4>
-            <ul class="wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">
-              <li><a href="javascript:void(0)">
-                <i class="fa fa-link" aria-hidden="true">&nbsp;</i> मन्त्रिपरिषद्को कार्यालय</a></li>
-              <li><a href="javascript:void(0)"><i class="fa fa-link" aria-hidden="true">&nbsp;</i> अर्थ मन्त्रालय</a></li>
-              <li><a href="javascript:void(0)"><i class="fa fa-external-link" aria-hidden="true">&nbsp;</i> लोक सेवा आयोग</a></li>
-              <li><a href="javascript:void(0)">
-                <i class="fa fa-anchor" aria-hidden="true">&nbsp;</i> राहदानी विभाग</a></li>
-              <li><a href="javascript:void(0)">
-                <i class="fa fa-file-text" aria-hidden="true">&nbsp;</i> मन्त्रिपरिषद्को कार्यालय</a></li>
-              <li><a href="javascript:void(0)">
-                <i class="fa fa-comments" aria-hidden="true">&nbsp;</i>
-              गृह मन्त्रालय</a></li>          </ul>
+            <h4>{{ $data['common']->footer_third_title }}</h4>
+            {!! $data['common']->footer_third_description !!}
           </div>
           <div class="col-lg-3 col-md-6 col-sm-6">
-            <h4>महत्त्वपूर्ण लिंकहरु</h4>
-            <ul class="wow fadeInUp" data-wow-duration="2s" data-wow-delay=".4s">
-              <li><a href="javascript:void(0)">
-                <i class="fa fa-link" aria-hidden="true">&nbsp;</i> नेपाल दूरसञ्चार प्राधिकरण</a></li>
-              <li><a href="javascript:void(0)">
-                <i class="fa fa-file-text" aria-hidden="true">&nbsp;</i> सूचना प्रविधि विभाग</a></li>
-              <li><a href="javascript:void(0)">
-               <i class="fa fa-comments" aria-hidden="true">&nbsp;</i> प्रमाणीकरण नियन्त्रकको कार्यालय</a></li>
-              <li><a href="javascript:void(0)">
-                <i class="fa fa-link" aria-hidden="true">&nbsp;</i> राष्ट्रिय सूचना प्रविधि केन्द्र</a></li>
-              <li><a href="javascript:void(0)">
-                <i class="fa fa-external-link" aria-hidden="true">&nbsp;</i>
-              सुचना प्रविधि मन्त्रालय</a></li>
-              <li><a href="javascript:void(0)">
-               <i class="fa fa-anchor" aria-hidden="true">&nbsp;</i> प्रमाणीकरण नियन्त्रकको कार्यालय</a></li>
-            </ul>
-          </div>
-          <div class="col-lg-3 col-md-6 col-sm-6">
+            <h4>{{ $data['common']->footer_fourth_title }}</h4>
+            {!! $data['common']->footer_fourth_description !!}
 
-          <div class="mapouter wow fadeInUp" data-wow-duration="2s" data-wow-delay=".5s">
-            <div class="gmap_canvas">
-              <iframe width="100%" height="280" id="gmap_canvas" src="https://maps.google.com/maps?q=Babar%20Mahal&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-            </div>
-          </div>
 
           </div>
           <div class="col-lg-12 col-md-12 col-sm-12"> </div>
