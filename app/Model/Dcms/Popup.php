722c7b1028338d645dc98f1eda83f50bcfe6f920
Lochan <krishnashrestha2052@gmail.com>
2019-08-18 22:06:12 +0545

    pop up MVC created

---
+++ app/Model/Dcms/Popup.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Dcms;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Popup extends Model
+{
+    //
+}
---
+++ database/migrations/2019_08_18_215959_create_popups_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePopupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('popups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('lang_id');
+            $table->string('popup_unique_id');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->text('image')->nullable();
+            $table->string('link')->nullable();
+            $table->integer('order')->nullable();
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('popups');
+    }
+}
