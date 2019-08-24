e46f7cc5093e8cd49ca5f1394e62fad24a5b76ac
Lochan <krishnashrestha49@gmail.com>
2019-08-24 11:47:00 -0700

    small change

--- resources/views/dcms/staff/sort.blade.php
+++ resources/views/dcms/staff/sort.blade.php
@@ -5,13 +5,11 @@
 @endsection 
 
 @section('content')
+@include('dcms.includes.breadcrumb')
 <div class="row">
     <div class="col-lg-12 col-md-12 col-xs-12">
-        <section class="panel">
-            <header class="panel-heading">
-               <h3>{{ $_panel }}</h3>
-            </header>
-            <div class="panel-body">
+        <section class="card">
+            <div class="card-body">
                     @include('dcms.includes.flash-message')  
                 @if(isset($data['branch']))                  
                     <form class="form-horizontal tasi-form" action="" method="get">
