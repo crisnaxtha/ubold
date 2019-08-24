1d01405b995b3a54b2e75b5da8d994eb0d5d751e
Lochan <krishnashrestha49@gmail.com>
2019-08-24 12:13:42 -0700

    delete of unwanted buttons

--- resources/views/dcms/includes/buttons/button-delete-branch.blade.php
+++
@@ -1,5 +0,0 @@
-@if(Auth::user()->role == "admin" || Auth::user()->role == "super-admin")
-@if(Route::has($_base_route.'.destroy'))
-    <button  id="delete" data-id="{{ $row->id }}" data-url="{{ URL::route($_base_route.'.destroy', ['id'=>$row->branch_unique_id]) }}" type="button" class="btn btn-xs btn-danger"><i class="far fa-trash-alt"></i></button>
-@endif
-@endif
