9b9a250fcab97e4bd56a193f654d2fd5ca9e11c1
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2019-09-22 08:03:57 +0545

    worded on featured stafff

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -44,7 +44,7 @@ class SiteController extends DM_BaseController
         $data['common'] = Common::where('lang_id', '=', $this->lang_id)->first();
         $data['menu'] = Menu::tree($this->lang_id);
         $data['imp_link'] = Link::where('status', '=', 1)->where('lang_id', '=', $this->lang_id)->get();
-        $data['member'] = Staff::where('featured', '=', 1)->where('lang_id', '=', $this->lang_id)->orderBy('level')->get();
+        $data['member'] = Staff::where('featured', '=', 1)->where('lang_id', '=', $this->lang_id)->orderBy('feature_order')->get();
 
         $data['album'] = $this->dm_post::joinAlbum($this->lang_id);
         $data['category'] = $this->dm_post::getCategoryList($this->lang_id);
