9ab24115c1022767f29b1bc683d3d8830e1df644
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2019-12-15 00:09:15 +0545

    deleted page done

--- resources/views/dcms/page/deleted.blade.php
+++ resources/views/dcms/page/deleted.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+@include('dcms.includes.breadcrumb')
 <!-- page start-->
 <div class="row">
     <div class="col-sm-12">
@@ -16,6 +17,23 @@
                @include('dcms.includes.buttons.button-back')
                <div class="btn-group pull-right">
                </div>
+               <div class="mb-2">
+                <div class="row">
+                    <div class="col-12 text-sm-center form-inline">
+                        <div class="form-group mr-2">
+                            <select id="demo-show-entries" class="form-control form-control-sm ml-1 mr-1">
+                                <option value="5">5</option>
+                                <option value="10">10</option>
+                                <option value="15">15</option>
+                                <option value="20">20</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input id="demo-foo-search" type="text" placeholder="Search" class="form-control form-control-sm" autocomplete="on">
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="adv-table">
                 <table  class="display table table-bordered table-striped" id="dynamic-table">
                    <thead>
--- resources/views/dcms/post/deleted.blade.php
+++ resources/views/dcms/post/deleted.blade.php
@@ -1,22 +1,40 @@
 @extends('dcms.layouts.app')
 @section('css')
        <!--dynamic table-->
-       @include('dcms.includes.datatable-assets.css')
+       @include('dcms.includes.footable-assets.css')
 
 @endsection
 
 @section('content')
 <!-- page start-->
+@include('dcms.includes.breadcrumb')
 <div class="row">
     <div class="col-sm-12">
-       <section class="card">
+       <section class="card-box">
           <header class="heading-title">
              Deleted {{ $_panel }}
           </header>
           <div class="card-body">
                 @include('dcms.includes.buttons.button-back')
-               <div class="btn-group float-right">
-               </div>
+                <div class="btn-group float-right">
+                </div>
+                <div class="mb-2">
+                    <div class="row">
+                        <div class="col-12 text-sm-center form-inline">
+                            <div class="form-group mr-2">
+                                <select id="demo-show-entries" class="form-control form-control-sm ml-1 mr-1">
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="15">15</option>
+                                    <option value="20">20</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input id="demo-foo-search" type="text" placeholder="Search" class="form-control form-control-sm" autocomplete="on">
+                            </div>
+                        </div>
+                    </div>
+                </div>
                <div class="table-responsive">
                     <table id="demo-foo-pagination" class="table table-bordered toggle-circle mb-0" data-page-size="7">
                         <thead>
@@ -59,39 +77,6 @@
                         </tfoot>
                     </table>
                 </div> <!-- end .table-responsive-->
-            <div class="adv-table">
-                <table  class="display table table-bordered table-striped" id="dynamic-table">
-                   <thead>
-                      <tr>
-                         <th>#</th>
-                         <th >Post Unique Id</th>
-                         <th>Category</th>
-                         <th class="hidden-phone">Deleted Date</th>
-                         <th>Title</th>
-                         <th class="hidden-phone">Action</th>
-                      </tr>
-                   </thead>
-                   <tbody>
-                     @if(isset($data['rows']))
-                        @foreach($data['rows'] as $row)
-                        <tr class="gradeX" id="{{ $row->id }}">
-                              <td width="25px">{{ $loop->iteration }}</td>
-                              <td width="25px">{{ $row->post_unique_id }}</td>
-                              <td width="25px">{{ $row->postCategory->name }}</td>
-                              <td width="25px" class="hidden-phone">
-                                    {{ $row->deleted_at }}
-                              </td>
-                              <td>{{ $row->title }}</td>
-                              <td width="150px" class="hidden-phone">
-                                 @include('dcms.includes.buttons.button-recycle-restore-post')
-                                 @include('dcms.includes.buttons.button-recycle-delete-post')
-                              </td>
-                           </tr>
-                        @endforeach
-                     @endif
-                   </tbody>
-                </table>
-             </div>
           </div>
        </section>
     </div>
@@ -101,6 +86,7 @@
 @section('js')
 @include('dcms.includes.flash-message')
 
-@include('dcms.includes.datatable-assets.js')
+@include('dcms.includes.footable-assets.js')
+
 
 @endsection
