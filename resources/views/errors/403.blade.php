08e3e4320ecb695a932c929eeec7065a6cf32b25
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2020-02-05 23:20:35 +0545

    worked on error page'

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,11 +1,51 @@
-@extends('errors::illustrated-layout')
+@extends('site.layouts.app')
+@section('css')
+<style>
+*{
+    transition: all 0.6s;
+}
 
-@section('code', '403')
-@section('title', __('Forbidden'))
+html {
+    height: 100%;
+}
 
-@section('image')
-<div style="background-image: url({{ asset('/svg/403.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+body{
+    font-family: 'Lato', sans-serif;
+    color: #888;
+    margin: 0;
+}
+
+#main{
+    display: table;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+}
+
+.fof{
+	  display: table-cell;
+	  vertical-align: middle;
+}
+
+.fof h1{
+	  font-size: 50px;
+	  display: inline-block;
+	  padding-right: 12px;
+	  animation: type .5s alternate infinite;
+}
+
+@keyframes type{
+	  from{box-shadow: inset -3px 0px 0px #888;}
+	  to{box-shadow: inset -3px 0px 0px transparent;}
+}
+</style>
+@endsection
+@section('content')
+
+<div id="main">
+    <div class="fof">
+            <h1>Error 403</h1>
+    </div>
+    <p>Sorry, you are forbidden from accessing this page.</p>
 </div>
 @endsection
-
-@section('message', __($exception->getMessage() ?: 'Sorry, you are forbidden from accessing this page.'))
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,87 @@
-@extends('errors::illustrated-layout')
+<!DOCTYPE html>
+<!-- saved from url=(0052)http://127.0.0.1:8000/en/dashboard/user_profile/edit -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-@section('code', '404')
-@section('title', __('Page Not Found'))
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('image')
-<div style="background-image: url({{ asset('/svg/404.svg') }});" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
-</div>
-@endsection
+        <title>Not Found</title>
 
-@section('message', __('Sorry, the page you are looking for could not be found.'))
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="http://fonts.gstatic.com/">
+        <link href="./Not Found_files/css" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .code {
+                border-right: 2px solid;
+                font-size: 26px;
+                padding: 0 15px 0 15px;
+                text-align: center;
+            }
+
+            .message {
+                font-size: 18px;
+                text-align: center;
+            }
+
+            /* vietnamese */
+@font-face {
+  font-family: 'Nunito';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIOuaBXso.woff2) format('woff2');
+  unicode-range: U+0102-0103, U+0110-0111, U+1EA0-1EF9, U+20AB;
+}
+/* latin-ext */
+@font-face {
+  font-family: 'Nunito';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofIO-aBXso.woff2) format('woff2');
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+  font-family: 'Nunito';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v10/XRXV3I6Li01BKofINeaB.woff2) format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="code">
+                404            </div>
+
+            <div class="message" style="padding: 10px;">
+                Not Found            </div>
+        </div>
+
+
+</body></html>
